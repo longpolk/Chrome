5aced0029ea1909c1838d506fcfc1dea6054f246
Long Nguyen <lnguyen209@csc.com>
2017-11-27 15:55:59 +0700

    test

--- Demo/Class1.cs
+++ Demo/Class1.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -25,6 +26,7 @@ namespace Demo
             //String strPath = Directory.GetCurrentDirector();
             //driver = new FirefoxDriver();
             driver = new ChromeDriver();
+            //driver = new InternetExplorerDriver();
             //verificationErrors = new StringBuilder();
         }
 
@@ -75,7 +77,29 @@ namespace Demo
             driver.FindElement(By.XPath("//input[@name='tripType'][@type='radio'][@value='oneway']")).Click();
             driver.FindElement(By.Name("findFlights")).Click();
 
+            driver.FindElement(By.XPath("//input[@name='outFlight'][@type='radio'][@value='Unified Airlines$363$281$11:24']")).Click();
+            driver.FindElement(By.XPath("//input[@name='inFlight'][@type='radio'][@value='Pangea Airlines$632$282$16:37']")).Click();
+            driver.FindElement(By.Name("reserveFlights")).Click();
 
+            driver.FindElement(By.Name("passFirst0")).Clear();
+            driver.FindElement(By.Name("passFirst0")).SendKeys("tutorial");
+            driver.FindElement(By.Name("passLast0")).Clear();
+            driver.FindElement(By.Name("passLast0")).SendKeys("tutorial");
+            new SelectElement(driver.FindElement(By.XPath("//select[@name='creditCard']"))).SelectByText("Visa");
+            new SelectElement(driver.FindElement(By.XPath("//select[@name='cc_exp_dt_mn']"))).SelectByText("01");
+            new SelectElement(driver.FindElement(By.XPath("//select[@name='cc_exp_dt_yr']"))).SelectByText("2000");
+            driver.FindElement(By.Name("cc_frst_name")).Clear();
+            driver.FindElement(By.Name("cc_frst_name")).SendKeys("tutorial");
+            driver.FindElement(By.Name("cc_mid_name")).Clear();
+            driver.FindElement(By.Name("cc_mid_name")).SendKeys("tutorial");
+            driver.FindElement(By.Name("cc_last_name")).Clear();
+            driver.FindElement(By.Name("cc_last_name")).SendKeys("tutorial");
+            driver.FindElement(By.XPath("//input[@type='checkbox'][@name='ticketLess']")).Click();
+            driver.FindElement(By.Name("billAddress2")).Clear();
+            driver.FindElement(By.Name("billAddress2")).SendKeys("tutorial");
+            new SelectElement(driver.FindElement(By.XPath("//select[@name='billCountry']"))).SelectByText("ASHMORE AND CARTIER ISLANDS ");
+            driver.FindElement(By.Name("buyFlights")).Click();
+            //driver.FindElement(By.XPath("//input[@type='checkbox'][@name='ticketLess']")).Click();
         }
         private bool IsElementPresent(By by)
         {
